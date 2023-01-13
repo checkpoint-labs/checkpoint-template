f44073170238f64134483bf8339327f11bcba0fc
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-01-13 14:03:34 +0100

    feat: track erc20 transfers

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import { getAddress } from '@ethersproject/address';
 import { BigNumber } from '@ethersproject/bignumber';
 import { shortStringArrToStr } from '@snapshot-labs/sx/dist/utils/strings';
+import { SplitUint256 } from '@snapshot-labs/sx/dist/utils/split-uint256';
 
 export const toAddress = bn => {
   try {
@@ -14,3 +15,29 @@ export const hexStrArrToStr = (data, start: number, length: number | bigint): st
   const dataSlice = data.slice(start, start + Number(length));
   return shortStringArrToStr(dataSlice.map(m => BigInt(m)));
 };
+
+export function getEvent(data: string[], format: string) {
+  const params = format.split(',').map(param => param.trim());
+  const event = {};
+  let len = 0;
+  let skip = 0;
+  params.forEach((param, i) => {
+    const name = param.replace('(uint256)', '').replace('(felt)', '').replace('(felt*)', '');
+    const next = i + skip;
+    if (len > 0) {
+      event[name] = data.slice(next, next + len);
+      skip += len - 1;
+      len = 0;
+    } else {
+      if (param.endsWith('(uint256)')) {
+        const uint256 = data.slice(next, next + 2);
+        event[name] = new SplitUint256(uint256[0], uint256[1]).toUint().toString();
+        skip += 1;
+      } else {
+        event[name] = data[next];
+      }
+    }
+    if (param.endsWith('_len')) len = parseInt(BigInt(data[next]).toString());
+  });
+  return event;
+}
--- src/writers.ts
+++ src/writers.ts
@@ -1,53 +1,27 @@
-import { hexStrArrToStr, toAddress } from './utils';
+import { getEvent, toAddress } from './utils';
 import type { CheckpointWriter } from '@snapshot-labs/checkpoint';
 
-export async function handleDeploy() {
-  // Run logic as at the time Contract was deployed.
-}
+export async function handleTransfer({
+  block,
+  tx,
+  event,
+  eventIndex,
+  mysql
+}: Parameters<CheckpointWriter>[0]) {
+  if (!event || !eventIndex) return;
 
-// This decodes the new_post events data and stores successfully
-// decoded information in the `posts` table.
-//
-// See here for the original logic used to create post transactions:
-// https://gist.github.com/perfectmak/417a4dab69243c517654195edf100ef9#file-index-ts
-export async function handleNewPost({ block, tx, event, mysql }: Parameters<CheckpointWriter>[0]) {
-  if (!event) return;
+  const format = 'from, to, value(uint256)';
+  const data: any = getEvent(event.data, format);
 
-  const author = toAddress(event.data[0]);
-  let content = '';
-  let tag = '';
-  const contentLength = BigInt(event.data[1]);
-  const tagLength = BigInt(event.data[2 + Number(contentLength)]);
-  const timestamp = block.timestamp;
-  const blockNumber = block.block_number;
-
-  // parse content bytes
-  try {
-    content = hexStrArrToStr(event.data, 2, contentLength);
-  } catch (e) {
-    console.error(`failed to decode content on block [${blockNumber}]: ${e}`);
-    return;
-  }
-
-  // parse tag bytes
-  try {
-    tag = hexStrArrToStr(event.data, 3 + Number(contentLength), tagLength);
-  } catch (e) {
-    console.error(`failed to decode tag on block [${blockNumber}]: ${e}`);
-    return;
-  }
-
-  // post object matches fields of Post type in schema.gql
-  const post = {
-    id: `${author}/${tx.transaction_hash}`,
-    author,
-    content,
-    tag,
-    tx_hash: tx.transaction_hash,
-    created_at: timestamp,
-    created_at_block: blockNumber
+  const transfer = {
+    id: `${tx.transaction_hash}/${eventIndex}`,
+    token: toAddress(event.from_address),
+    from: data.from,
+    to: data.to,
+    value: data.value,
+    created_at: block.timestamp,
+    created_at_block: block.block_number
   };
 
-  // table names are `lowercase(TypeName)s` and can be interacted with sql
-  await mysql.queryAsync('INSERT IGNORE INTO posts SET ?', [post]);
+  await mysql.queryAsync('INSERT IGNORE INTO transfers SET ?', [transfer]);
 }
