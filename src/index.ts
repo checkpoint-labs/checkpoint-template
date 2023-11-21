078361b9a1d27e178c32aaa565a66e50de2e8646
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-11-21 21:58:24 +0100

    feat: index EVM events

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,33 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import fs from 'fs';
-import Checkpoint, { LogLevel } from '@snapshot-labs/checkpoint';
+import Checkpoint, { LogLevel, evm } from '@snapshot-labs/checkpoint';
 import config from './config.json';
 import * as writers from './writers';
-import checkpointBlocks from './checkpoints.json';
+import ProxyFactory from './abis/ProxyFactory.json';
 
 const dir = __dirname.endsWith('dist/src') ? '../' : '';
 const schemaFile = path.join(__dirname, `${dir}../src/schema.gql`);
 const schema = fs.readFileSync(schemaFile, 'utf8');
 
-const checkpointOptions = {
-  logLevel: LogLevel.Info
-  // prettifyLogs: true, // uncomment in local dev
-};
+const indexer = new evm.EvmIndexer(writers);
 
 // Initialize checkpoint
-// @ts-ignore
-const checkpoint = new Checkpoint(config, writers, schema, checkpointOptions);
+const checkpoint = new Checkpoint(config, indexer, schema, {
+  logLevel: LogLevel.Info,
+  resetOnConfigChange: true,
+  abis: {
+    ProxyFactory
+  }
+  // prettifyLogs: true, // uncomment in local dev
+});
 
 // resets the entities already created in the database
 // ensures data is always fresh on each re-run
-checkpoint
-  .reset()
-  .then(() => checkpoint.seedCheckpoints(checkpointBlocks))
-  .then(() => {
-    // start the indexer
-    checkpoint.start();
-  });
+checkpoint.reset().then(() => {
+  // start the indexer
+  checkpoint.start();
+});
 
 const app = express();
 app.use(express.json({ limit: '4mb' }));
--- src/writers.ts
+++ src/writers.ts
@@ -1,53 +1,7 @@
-import { hexStrArrToStr, toAddress } from './utils';
-import type { CheckpointWriter } from '@snapshot-labs/checkpoint';
+import { evm } from '@snapshot-labs/checkpoint';
 
-export async function handleDeploy() {
-  // Run logic as at the time Contract was deployed.
-}
-
-// This decodes the new_post events data and stores successfully
-// decoded information in the `posts` table.
-//
-// See here for the original logic used to create post transactions:
-// https://gist.github.com/perfectmak/417a4dab69243c517654195edf100ef9#file-index-ts
-export async function handleNewPost({ block, tx, event, mysql }: Parameters<CheckpointWriter>[0]) {
+export const handleProxyDeployed: evm.Writer = async ({ block, event }) => {
   if (!block || !event) return;
 
-  const author = toAddress(event.data[0]);
-  let content = '';
-  let tag = '';
-  const contentLength = BigInt(event.data[1]);
-  const tagLength = BigInt(event.data[2 + Number(contentLength)]);
-  const timestamp = block.timestamp;
-  const blockNumber = block.block_number;
-
-  // parse content bytes
-  try {
-    content = hexStrArrToStr(event.data, 2, contentLength);
-  } catch (e) {
-    console.error(`failed to decode content on block [${blockNumber}]: ${e}`);
-    return;
-  }
-
-  // parse tag bytes
-  try {
-    tag = hexStrArrToStr(event.data, 3 + Number(contentLength), tagLength);
-  } catch (e) {
-    console.error(`failed to decode tag on block [${blockNumber}]: ${e}`);
-    return;
-  }
-
-  // post object matches fields of Post type in schema.gql
-  const post = {
-    id: `${author}/${tx.transaction_hash}`,
-    author,
-    content,
-    tag,
-    tx_hash: tx.transaction_hash,
-    created_at: timestamp,
-    created_at_block: blockNumber
-  };
-
-  // table names are `lowercase(TypeName)s` and can be interacted with sql
-  await mysql.queryAsync('INSERT IGNORE INTO posts SET ?', [post]);
-}
+  console.log('deployed', event.args.implementation, event.args.proxy);
+};
