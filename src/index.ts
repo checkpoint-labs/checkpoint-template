14aab74fc47ce87f7688fff161ac22ff683c42d3
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-11-21 21:58:24 +0100

    feat: index EVM events

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,30 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import fs from 'fs';
-import Checkpoint, { LogLevel } from '@snapshot-labs/checkpoint';
+import Checkpoint, { LogLevel, evm } from '@snapshot-labs/checkpoint';
 import config from './config.json';
 import * as writers from './writers';
-import checkpointBlocks from './checkpoints.json';
-import Poster from './abis/Poster.json';
+import ProxyFactory from './abis/ProxyFactory.json';
 
 const dir = __dirname.endsWith('dist/src') ? '../' : '';
 const schemaFile = path.join(__dirname, `${dir}../src/schema.gql`);
 const schema = fs.readFileSync(schemaFile, 'utf8');
 
-const checkpoint = new Checkpoint(config, writers, schema, {
+const indexer = new evm.EvmIndexer(writers);
+
+// Initialize checkpoint
+const checkpoint = new Checkpoint(config, indexer, schema, {
   logLevel: LogLevel.Info,
-  prettifyLogs: true,
-  fetchInterval: 15000,
+  resetOnConfigChange: true,
   abis: {
-    Poster
-  }
+    ProxyFactory
+  },
+  prettifyLogs: true // uncomment in local dev
 });
 
 async function run() {
   await checkpoint.reset();
   await checkpoint.resetMetadata();
-  await checkpoint.seedCheckpoints(checkpointBlocks);
   await checkpoint.start();
 }
 
--- src/writers.ts
+++ src/writers.ts
@@ -1,22 +1,7 @@
-import { validateAndParseAddress } from 'starknet';
-import { CheckpointWriter } from '@snapshot-labs/checkpoint';
-import { Post } from '../.checkpoint/models';
-import { longStringToText } from './utils';
+import { evm } from '@snapshot-labs/checkpoint';
 
-export const handleNewPost: CheckpointWriter = async ({ block, tx, rawEvent, event }) => {
-  if (!block || !event || !rawEvent) return;
+export const handleProxyDeployed: evm.Writer = async ({ block, event }) => {
+  if (!block || !event) return;
 
-  const author = validateAndParseAddress(rawEvent.from_address);
-  const content = longStringToText(event.content);
-  const tag = longStringToText(event.tag);
-
-  const post = new Post(`${author}/${tx.transaction_hash}`);
-  post.author = author;
-  post.content = content;
-  post.tag = tag;
-  post.tx_hash = tx.transaction_hash;
-  post.created_at = block.timestamp;
-  post.created_at_block = block.block_number;
-
-  await post.save();
+  console.log('deployed', event.args.implementation, event.args.proxy);
 };
