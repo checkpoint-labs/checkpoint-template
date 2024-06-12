87b2a91dc751bf38c096bf6e69c191280657a25a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2024-06-12 11:28:03 +0200

    feat: add more complex relations

--- src/writers.ts
+++ src/writers.ts
@@ -1,6 +1,6 @@
 import { validateAndParseAddress } from 'starknet';
 import { starknet } from '@snapshot-labs/checkpoint';
-import { Post } from '../.checkpoint/models';
+import { Post, User, UserInfo, Tag } from '../.checkpoint/models';
 import { longStringToText } from './utils';
 
 export const handleNewPost: starknet.Writer = async ({ block, tx, rawEvent, event }) => {
@@ -8,12 +8,34 @@ export const handleNewPost: starknet.Writer = async ({ block, tx, rawEvent, even
 
   const author = validateAndParseAddress(rawEvent.from_address);
   const content = longStringToText(event.content);
-  const tag = longStringToText(event.tag);
+  const tagValue = longStringToText(event.tag);
+
+  let user = await User.loadEntity(author);
+  if (!user) {
+    user = new User(author);
+    user.info = author;
+    user.created_at = block.timestamp;
+    user.created_at_block = block.block_number;
+    await user.save();
+  }
+
+  let userInfo = await UserInfo.loadEntity(author);
+  if (!userInfo) userInfo = new UserInfo(author);
+  userInfo.post_count += 1;
+  await userInfo.save();
+
+  let tag = await Tag.loadEntity(tagValue);
+  if (!tag) {
+    tag = new Tag(tagValue);
+    tag.created_at = block.timestamp;
+    tag.created_at_block = block.block_number;
+    await tag.save();
+  }
 
   const post = new Post(`${author}/${tx.transaction_hash}`);
   post.author = author;
   post.content = content;
-  post.tag = tag;
+  post.tags = [tagValue];
   post.tx_hash = tx.transaction_hash;
   post.created_at = block.timestamp;
   post.created_at_block = block.block_number;
